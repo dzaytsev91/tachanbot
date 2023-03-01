d70fd09f5b2aa92965914494f722d7df1cb0446b
Logovskii Dmitrii <dlogovskiy@ozon.ru>
2023-03-01 10:41:25 +0300

    add votes

---
+++ cron_job.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+from datetime import datetime, timedelta
+
+import telebot
+
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+
+conn = sqlite3.connect("memes.db", check_same_thread=False)
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS posts (id integer PRIMARY KEY, up_votes int, down_votes int, created_at timestamp,message_id int);"
+)
+
+
+def getKey(x):
+    return [x[1], -x[2]]
+
+
+def main():
+    chat_id = -891469087
+    seven_days_ago = datetime.now() - timedelta(days=7)
+    query = "SELECT * FROM posts WHERE created_at > ?"
+    rows = conn.execute(query, (seven_days_ago,)).fetchall()
+    rows.sort(key=getKey)
+    first, second, third = None, None, None
+    for row in reversed(rows):
+        if first is None:
+            bot.send_message(chat_id, "🥇", reply_to_message_id=row[4])
+            first = 1
+            continue
+        if second is None:
+            bot.send_message(chat_id, "🥈", reply_to_message_id=row[4])
+            second = 1
+            continue
+        if third is None:
+            bot.send_message(chat_id, "🥉", reply_to_message_id=row[4])
+            third = 1
+            continue
+        return
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import os
+import sqlite3
+from datetime import datetime
 
 import telebot
 
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
+conn = sqlite3.connect("memes.db", check_same_thread=False)
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS posts (id integer PRIMARY KEY, up_votes int, down_votes int, created_at timestamp,message_id int);"
+)
 
-@bot.message_handler(content_types=['photo','video'])
+
+def test(data):
+    cursor = conn.cursor()
+    query = "INSERT INTO posts (id, up_votes, down_votes) VALUES(?, ?, ?) ON CONFLICT(id) DO UPDATE SET up_votes={up_votes}, down_votes={down_votes};".format(
+        up_votes=data.options[0].voter_count, down_votes=data.options[1].voter_count
+    )
+    cursor.execute(
+        query, (data.id, data.options[0].voter_count, data.options[1].voter_count)
+    )
+    conn.commit()
+
+
+@bot.message_handler(content_types=["photo", "video"])
+@bot.poll_handler(test)
 def send_rand_photo(message):
-    bot.send_poll(message.chat.id, "Норм?", ['👍', "👎"])
+    poll_data = bot.send_poll(message.chat.id, "Норм?", ["👍", "👎"])
+    query = "INSERT INTO posts (id, created_at, message_id, up_votes, down_votes) VALUES(?, ?, ?, ?, ?) ON CONFLICT(id) DO NOTHING;"
+    cursor = conn.cursor()
+    cursor.execute(query, (poll_data.poll.id, datetime.now(), poll_data.id - 1, 0, 0))
+    conn.commit()
 
 
 def main():
     bot.infinity_polling(allowed_updates=telebot.util.update_types)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
