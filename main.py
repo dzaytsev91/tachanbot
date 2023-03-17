3802459d8b722b6f8f9654efaf0670a30582a3a9
Logovskii Dmitrii <dlogovskiy@ozon.ru>
2023-03-17 15:53:36 +0300

    add soul

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def send_rand_photo(message):
         if message.text.lower() == "нет":
             bot.send_message(
                 message.chat.id,
-                "Пидора ответ",
+                "Пидора ответ :)",
                 reply_to_message_id=message.id,
                 message_thread_id=message.message_thread_id,
             )
