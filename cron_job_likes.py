f2839c01b6ec081cd3aefa50574d4a1d86946fa2
Logovskii Dmitrii <dlogovskiy@ozon.ru>
2023-04-24 10:49:38 +0300

    new cron likes

--- cron_job_likes.py
+++ cron_job_likes.py
@@ -14,19 +14,28 @@ conn = sqlite3.connect("memes.db", check_same_thread=False)
 
 def main():
     seven_days_ago = datetime.now() - timedelta(days=7)
-    query = "SELECT user_id, MAX(username), SUM(up_votes), SUM(down_votes) FROM memes_posts WHERE created_at > ? GROUP BY user_id ORDER BY 3 DESC,4 DESC LIMIT 3"
+    query = "SELECT user_id, MAX(username), SUM(up_votes), SUM(down_votes) FROM memes_posts WHERE created_at > ? GROUP BY user_id ORDER BY 3 DESC,4 DESC"
     rows = conn.execute(query, (seven_days_ago,)).fetchall()
     msg = []
     stack = ["🥉", "🥈", "🥇"]
     for row in rows:
         user_id, username, up_votes, down_votes = row
-        msg.append(
-            "["
-            + username
-            + "](tg://user?id="
-            + str(user_id)
-            + ") 👍 {}, 👎 {} - {}".format(up_votes, down_votes, stack.pop())
-        )
+        if stack:
+            msg.append(
+                "["
+                + username
+                + "](tg://user?id="
+                + str(user_id)
+                + ") 👍 {}, 👎 {} - {}".format(up_votes, down_votes, stack.pop())
+            )
+        else:
+            msg.append(
+                "["
+                + username
+                + "](tg://user?id="
+                + str(user_id)
+                + ") 👍 {}, 👎 {} - 💩".format(up_votes, down_votes)
+            )
     bot.send_message(
         memes_chat_id,
         "\n".join(msg),
