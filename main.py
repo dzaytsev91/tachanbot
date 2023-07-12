6ce5eaaa7588c5afbe910f63e326d8dbdeda042a
Logovskii Dmitrii <dlogovskiy@ozon.ru>
2023-07-12 16:39:18 +0300

    count user messages

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ memes_chat_link_id = int(os.getenv("MEMES_CHAT_LINK_ID", 1))
 
 conn = sqlite3.connect("memes.db", check_same_thread=False)
 conn.execute(
-    "CREATE TABLE IF NOT EXISTS posts (hash string, message_id int, message_thread_id int, user_id int);"
-)
-conn.execute(
     "CREATE TABLE IF NOT EXISTS memes_posts (id integer PRIMARY KEY, up_votes int, down_votes int, created_at timestamp,message_id int, user_id int, username string, old_hat_votes int);"
 )
 conn.execute(
-    "CREATE TABLE IF NOT EXISTS user_messages (username string, message_id int, message_thread_id int, user_id int, created_at timestamp);"
+    "CREATE TABLE IF NOT EXISTS user_messages (user_id int, message_id int, message_thread_id int, created_at timestamp);"
+)
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS users (user_id int, username string, active bool, joined_date timestamp);"
 )
 
 
@@ -160,15 +160,14 @@ def get_statistic(message):
         "contact",
     ]
 )
-def send_rand_photo(message):
+def handle_message(message):
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO user_messages (username, message_id, message_thread_id, user_id, created_at) VALUES(?, ?, ?, ?, ?) ON CONFLICT DO NOTHING",
+        "INSERT INTO user_messages (user_id, message_id, message_thread_id, created_at) VALUES(?, ?, ?, ?) ON CONFLICT DO NOTHING",
         (
-            message.from_user.first_name,
+            message.from_user.id,
             message.id,
             message.message_thread_id,
-            message.from_user.id,
             datetime.now(),
         ),
     )
@@ -191,30 +190,12 @@ def send_rand_photo(message):
     ):
         bot.delete_message(message.chat.id, message.id)
     else:
-        proccess_photo_mem(message)
         if message.media_group_id:
             if not ttl_cache.get(message.media_group_id):
                 ttl_cache[message.media_group_id] = 1
                 create_pool(message)
         else:
             create_pool(message)
-
-
-def proccess_photo_mem(message):
-    if not message.photo:
-        return
-    for photo in message.photo:
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO posts (hash, message_id, message_thread_id, user_id) VALUES(?, ?, ?, ?) ON CONFLICT DO NOTHING",
-            (
-                photo.file_unique_id,
-                message.id,
-                message.message_thread_id,
-                message.from_user.id,
-            ),
-        )
-        conn.commit()
 
 
 @bot.message_handler(content_types=["new_chat_members"])
@@ -232,10 +213,27 @@ def hello(message):
             message_thread_id=message.message_thread_id,
             parse_mode="Markdown",
         )
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO users (user_id, username, active, joined_date) VALUES(?, ?, ?, ?) ON CONFLICT DO UPDATE SET active=1",
+            (
+                user_id,
+                user_name,
+                True,
+                datetime.now(),
+            ),
+        )
+        conn.commit()
 
 
 @bot.message_handler(content_types=["left_chat_member"])
 def goodbye(message):
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE users SET active=0 WHERE user_id=?",
+        (message.from_user.id,),
+    )
+    conn.commit()
     bot.send_message(
         message.chat.id,
         random.choice(
